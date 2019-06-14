4c863be013da0eeda3862ad9aba3c38119fd1807
eledelove <51303013+eledelove@users.noreply.github.com>
2019-06-14 08:30:29 -0500

    Nuevo Analisis

---
+++ sintactico.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class cAnalisisSintactico{
+
+void programa(){
+    secuencia_sent();
+}
+
+void secuencia_sent(){
+
+}
+
+void sentencia_if(){
+
+}
+
+void sentencia_repeat(){
+
+}
+void sentencia_asignacion(){
+
+}
+void sentencia_read(){
+
+}
+void sentencia_writet(){
+
+}
+void expresion(){
+
+}
+void exp_simple(){
+
+}
+void termino(){
+
+}
+void factor(){
+    if(token == '(') expresion();
+    if(toekn == '(') expresion();
+
+    else if(token == num){
+
+        obtenSigToken();
+    }
+    else if(token == id){
+
+        obtenSigToken();
+    }
+}
+
+};
+
+int main(){
+
+    cout<<"Hola Ionhatan" <<endl;
+    return 0;
+}

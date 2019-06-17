541d6be5648db5f9bd2f1b083e55b65a61fa20c1
eledelove <51303013+eledelove@users.noreply.github.com>
2019-06-17 08:24:21 -0500

    Add files via upload

--- sintactico.cpp
+++ sintactico.cpp
@@ -3,11 +3,44 @@ using namespace std;
 
 class cAnalisisSintactico{
 
+string tokenActual;
+int i;
+string tokens[30];
+
+public:
+//constructor
+cAnalisisSintactico(){
+
+    i=0;
+    tokenActual="";
+    recuperaTokens();
+
+}
+
+void recuperaTokens(){
+    //llenar el arreglo tokens
+}
+
+void match(string tokenEsperado){
+
+    if(tokenActual == tokenEsperado)
+        tokenActual=obtenSigToken();
+
+    else cerr<<"Error";
+}
+
+string obtenSigToken(){
+
+    return tokens[++i];
+}
+
+
 void programa(){
     secuencia_sent();
 }
 
 void secuencia_sent(){
+    secuencia_sent();
 
 }
 
@@ -28,27 +61,51 @@ void sentencia_writet(){
 
 }
 void expresion(){
+    exp_simple();
+    if(tokenActual == "=" || tokenActual == "<")
+        exp_simple();
 
 }
 void exp_simple(){
 
+    termino();
+    tokenActual = obtenSigToken();
+    if(tokenActual == "+" || tokenActual == "-")
+        APrima();
+
 }
+
+void APrima(){
+    if(tokenActual == "+" || tokenActual == "-"){
+        match(tokenActual);
+        termino();
+    }
+    if(tokenActual == "+" || tokenActual == "-")
+    APrima();
+}
+
 void termino(){
 
 }
 void factor(){
-    if(token == '(') expresion();
-    if(toekn == '(') expresion();
+    if(tokenActual == "("){
+        match("(");
+        expresion();
+        match(")");
 
-    else if(token == num){
-
-        obtenSigToken();
     }
-    else if(token == id){
 
-        obtenSigToken();
+    else if(isalpha(tokenActual[0]) || tokenActual[0] == '_'){
+
+
     }
+    else if(isdigit(tokenActual[0])){
+
+    }
+    else cerr<<"Error en el factor";
 }
+
+
 
 };
 
